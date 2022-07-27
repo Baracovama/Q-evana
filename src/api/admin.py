c32b372692b87a18b00ff1b414018c6f20ec1437
Baracovama <juanvguevaram@gmail.com>
2022-07-27 00:11:59 +0000

    Base de datos

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Favourites_Sagas, Favourites_Peliculas, Sagas, Sagapeli, Peliculas
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,12 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Favourites_Sagas, db.session))
+    admin.add_view(ModelView(Favourites_Peliculas, db.session))
+    admin.add_view(ModelView(Sagas, db.session))
+    admin.add_view(ModelView(Sagapeli, db.session))
+    admin.add_view(ModelView(Peliculas, db.session))
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -6,9 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    user_name = db.Column(db.String(250), unique=True, nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
     name = db.Column(db.String(300), unique=False, nullable=False)
-    favourites = db.relationship('Favourites', backref = 'User')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -18,62 +17,91 @@ class User(db.Model):
            "id": self.id,
             "name": self.name,
             "email": self.email,
-            #"user_name": self.user_name,
-            #"favourites": self.favourites,
+            "username": self.username,
             # do not serialize the password, its a security breach
         }
  
-class Favourites(db.Model):
+class Favourites_Sagas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(250), unique=True, nullable=False)
-    id_series = db.Column(db.String(250), unique=True, nullable=False)
-    id_sagas = db.Column(db.String(250), unique=True, nullable=False)
-    id_peliculas = db.Column(db.String(250), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User)
+    saga_id = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<Favourites_sagas {self.user_id}>'
 
-class Studio(db.model):
-    id = db.Column(db.Integer,primary_key=True)
-    studio = db.Column(db.String(250), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "saga_id": self.saga_id,
+        }
 
-class Category(db.model):
-    id = db.Column(db.Integer,primary_key=True)
-    category = db.Column(db.String(250), unique=True, nullable=False)
-
-class Cast(db.model):
-    id = db.Column(db.Integer,primary_key=True)
-    real_name = db.Column(db.String(250), unique=True, nullable=False)
-    caracter = db.Column(db.String(250), unique=True, nullable=False)
-    photo = db.Column(db.String(250), unique=True, nullable=False)
-
-class Peliculas(db.Model):
+class Favourites_Peliculas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    description = db.Column(db.String(250), unique=True, nullable=False)
-    category = db.Column(db.String(250), unique=True, nullable=False)
-    date = db.Column(db.String(250), unique=True, nullable=False)
-    valoration = db.Column(db.String(250), unique=True, nullable=False)
-    cast = db.Column(db.String(250), unique=True, nullable=False)
-    studio = db.Column(db.String(250), unique=True, nullable=False)
-    duration = db.Column(db.Integer),
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User)
+    pelicula_id = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'<Favourites_peliculas {self.user_id}>'
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "pelicula_id": self.pelicula_id,
+        }
 
 class Sagas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    description = db.Column(db.String(250), unique=True, nullable=False)
-    category = db.Column(db.String(250), unique=True, nullable=False)
-    date = db.Column(db.String(250), unique=True, nullable=False)
-    valoration = db.Column(db.String(250), unique=True, nullable=False)
-    cast = db.Column(db.String(250), unique=True, nullable=False)
-    studio = db.Column(db.String(250), unique=True, nullable=False)
-    movies = db.Column(db.String(250), unique=True, nullable=False)
+    saga_peli_id = db.Column(db.Integer, db.ForeignKey('sagapeli.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Sagas {self.saga_peli_id}>'
 
-class Series(db.Model):
+    def serialize(self):
+        return {
+            "saga_peli_id": self.saga_peli_id,
+        }
+
+class Sagapeli(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     description = db.Column(db.String(250), unique=True, nullable=False)
-    category = db.Column(db.String(250), unique=True, nullable=False)
-    date = db.Column(db.String(250), unique=True, nullable=False)
-    valoration = db.Column(db.String(250), unique=True, nullable=False)
-    cast = db.Column(db.String(250), unique=True, nullable=False)
-    studio = db.Column(db.String(250), unique=True, nullable=False)
-    ntemporadas = db.Column(db.Integer),
-    nepisodios = db.Column(db.Integer),
+    category_id = db.Column(db.Integer)
+    #date = db.Column(db.Datetime)
+    valoration = db.Column(db.Integer)
+    cast_id = db.Column(db.Integer)
+    studio_id = db.Column(db.Integer)
+    duration = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Sagapeli {self.title}>'
+
+    def serialize(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "category_id": self.category_id,
+            #"date": self.date,
+            "valoration": self.valoration,
+            "cast_id": self.cast_id,
+            "studio_id": self.studio_id,
+            "duration": self.duration,
+        }
+
+class Peliculas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    saga_peli_id = db.Column(db.Integer, db.ForeignKey('sagapeli.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Peliculas {self.saga_peli_id}>'
+
+    def serialize(self):
+        return {
+            "saga_peli_id": self.saga_peli_id,
+        }
+
+'''class Category(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)'''

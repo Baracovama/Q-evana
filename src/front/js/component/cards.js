4449ecab813e86dc9605b48da96b035c09c25f7e
Baracovama <juanvguevaram@gmail.com>
2022-08-13 10:52:08 +0000

    Comienzo de las cards buenas

--- src/front/js/component/cards.js
+++ src/front/js/component/cards.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Cards = () => {
-    return (
-        <div className="card">
-                <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=4420fdc66e8fbaa810cbb4c5a36fb67c&language=es&page=",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setData(result.results))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <div className="card">
+      {" "}
+      <img src="..." className="card-img-top" alt="..." />{" "}
+      <div className="card-body">
+        {" "}
+        <h5 className="card-title"> Card title </h5>{" "}
+        <p className="card-text">
+          {" "}
+          {data.map((item, i) => (
+            <li> {item.id} </li>
+          ))}{" "}
+          Some quick example text to build on the card title and make up the
+          bulk of the card 's content.{" "}
+        </p>{" "}
+        <a href="#" class="btn btn-primary">
+          {" "}
+          Go somewhere{" "}
+        </a>{" "}
+      </div>{" "}
+    </div>
+  );
 };

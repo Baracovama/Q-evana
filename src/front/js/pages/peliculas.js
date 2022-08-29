28ee7ddcaa26deeeb7f1bb4003b7ea8f99b1f37c
Baracovama <juanvguevaram@gmail.com>
2022-08-29 02:13:06 +0000

    errores y mas errores

--- src/front/js/pages/peliculas.js
+++ src/front/js/pages/peliculas.js
@@ -2,49 +2,64 @@ import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/cards.css";
+import styles from "../../styles/peliculas.css";
 
 export const Peliculas = () => {
-  const params = useParams();
+  const { id } = useParams();
   const { store, actions } = useContext(Context);
+  const [pelicula, setPelicula] = useState(null);
 
   useEffect(() => {
-    actions.details(params.id);
-  }, []);
+    fetch(process.env.BACKEND_URL + "/api/pelicula" + id), {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    }})
+    .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setPelicula(data)
+      })
+  }, [pelicula]);
+
+  if (!pelicula) {
+    return null;
+  }
 
   return (
     <>
-      <div className="container mb-5 ">
-        <div className="row g-0">
+      <div className="container mb-1 ">
+        <div className="row">
           {store.peliculon.length > 0 ? (
             store.peliculon.map((item, index) => {
               return (
-                <div
-                  className="card-pelis m-1 mb-3"
-                  style="max-width: 540px;"
-                  key={index}
-                >
-                  <div className="blog-posts">
-                    <div className="post">
-                      <img
-                        src={
-                          "https://image.tmdb.org/t/p/w500" + item.poster_path
-                        }
-                        className="post-img"
-                        alt="..."
-                      />
-                      <div className="post-content">
-                        <h5 className="card-title">{item.title}</h5>
-                        <p className="card-text">{item.release_date}</p>
-                        <p className="card-text">{item.vote_average}</p>
-                      </div>
-                    </div>
+                <div className={styles.detailsContainer}>
+                  <img
+                    className={`${styles.col} ${styles.peliculaImagen}`}
+                    src={"https://image.tmdb.org/t/p/w500" + item.poster_path}
+                  />
+                  <div className={`${styles.col} ${styles.peliculaDetalles}`}>
+                    <p className={styles.title}>
+                      <strong>Titulo: </strong>
+                      {pelicula.title}
+                    </p>
+                    <p>
+                      <strong>Generos: </strong>
+                      {item.genres.map((genre) => genre.name).join(", ")}
+                    </p>
+                    <p>
+                      <strong>Descripcion: </strong>
+                      {item.overview}
+                    </p>
                   </div>
                 </div>
               );
             })
           ) : (
-            <section class="area">
-              <div class="ball"></div>
+            <section className="area">
+              <div className="ball"></div>
             </section>
           )}
         </div>

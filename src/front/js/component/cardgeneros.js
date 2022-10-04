63d21ceb3f4cec34610e9fbf79260f52ecaefd12
Baracovama <juanvguevaram@gmail.com>
2022-10-04 23:53:50 +0000

    arreglos en cards de generos

--- src/front/js/component/cardgeneros.js
+++ src/front/js/component/cardgeneros.js
@@ -16,10 +16,9 @@ import { Link } from "react-router-dom";
 export const CardGeneros = () => {
   const { store, actions } = useContext(Context);
 
-  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(actions.generoslist());
-  }, [dispatch]);
+    actions.generoslist();
+  }, []);
 
   return (
     <div className=" tv_slide ">
@@ -36,33 +35,3 @@ export const CardGeneros = () => {
     </div>
   );
 };
-
-// import React, { useState, useEffect, useContext } from "react";
-// import { Context } from "../store/appContext";
-// import "../../styles/cardgeneros.css";
-// import { Link } from "react-router-dom";
-
-// export const CardGeneros = () => {
-//   const { store, actions } = useContext(Context);
-
-//   useEffect(() => {
-//     actions.generoslist();
-//   }, []);
-
-//   return (
-//     <div className=" container mb-5  ">
-//       <h1 className="text-white">Generos</h1>
-//       <div className="row flex-row flex-nowrap overflow-auto">
-//         {store.generos.map((item, index) => {
-//           return (
-//             <div className="card-generos  m-2" style={{ width: "20rem" }}>
-//               <Link to={/peliculas/genero/${item.id}}>
-//                 <h2>{item.name}</h2>
-//               </Link>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };

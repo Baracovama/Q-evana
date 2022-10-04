5de3cbc262aea2eba63cc1458ca29956381cea2c
Baracovama <juanvguevaram@gmail.com>
2022-10-04 22:37:35 +0000

    Footer

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -24,110 +24,110 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light">
       {store.auth ? (
         <div className="container-fluid">
-        <a
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </a>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="#"></a>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/">
-                <div
-                  className="nav-link active text-light"
-                  aria-current="page"
-                >
-                  <img src="logo.png" className="logo" />
-                </div>
-              </Link>
-            </li>
-            <li className="nav-item categorias">
-              <a className="nav-link text-light" href="#">
-                <i className="fas fa-film"> Populares</i>
-              </a>
-            </li>
-            <li className="nav-item categorias">
-              <a className="nav-link text-light" href="#">
-                <i className="fas fa-medal"> Mejor Valoradas</i>
-              </a>
-            </li>
-            <li className="nav-item categorias">
+          <a
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="#"></a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/">
+                  <div
+                    className="nav-link active text-light"
+                    aria-current="page"
+                  >
+                    <img src="logo.png" className="logo" />
+                  </div>
+                </Link>
+              </li>
+              <li className="nav-item categorias">
+                <a className="nav-link text-light" href="#">
+                  <i className="fas fa-film"> Populares</i>
+                </a>
+              </li>
+              <li className="nav-item categorias">
+                <a className="nav-link text-light" href="#">
+                  <i className="fas fa-medal"> Mejor Valoradas</i>
+                </a>
+              </li>
+              <li className="nav-item categorias">
                 <Link to={"/favoritos"} className="nav-link text-light">
                   <i class="fas fa-plus"> Mi lista</i>
                 </Link>
-            </li>
-            <li className="nav-item categorias">
-              <Link className="nav-link text-light" to="/contacta">
-                <i className="fas fa-phone"> Contactanos</i>
-              </Link>
-            </li>
-          </ul>
-          <div className="d-flex" role="search">
-            <div className="formInput me-2">
-              <button className="botonInput">
-                <svg
-                  width="17"
-                  height="16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-labelledby="search"
-                >
-                  <path
-                    d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
-                    stroke="currentColor"
-                    stroke-width="1.333"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
+              </li>
+              <li className="nav-item categorias">
+                <Link className="nav-link text-light" to="/contacta">
+                  <i className="fas fa-phone"> Contactanos</i>
+                </Link>
+              </li>
+            </ul>
+            <div className="d-flex" role="search">
+              <div className="formInput me-2">
+                <button className="botonInput">
+                  <svg
+                    width="17"
+                    height="16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-labelledby="search"
+                  >
+                    <path
+                      d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+                      stroke="currentColor"
+                      stroke-width="1.333"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </button>
+                <input
+                  value={busqueda}
+                  placeholder="Search..."
+                  required=""
+                  type="text"
+                  onChange={(e) => setBusqueda(e.target.value)}
+                  onKeyPress={(e) => handlesubmit(e)}
+                  className="form-control me-2 "
+                />
+              </div>
+            </div>
+            <div className="me-2">
+              <button
+                className="noselect botonLogOut"
+                onClick={() => {
+                  const desconecto = actions.LogOut();
+                  if (desconecto) {
+                    navigate(0);
+                  }
+                }}
+              >
+                <span className="text">
+                  {store.username ? store.username : ""}
+                </span>
+                <span className="icon">
+                  {" "}
+                  <i className="fas fa-sign-out-alt"></i>{" "}
+                </span>
               </button>
-              <input
-                value={busqueda}
-                placeholder="Search..."
-                required=""
-                type="text"
-                onChange={(e) => setBusqueda(e.target.value)}
-                onKeyPress={(e) => handlesubmit(e)}
-                className="form-control me-2 "
-              />
+            </div>
+            <div className="me-2">
+              <Link to={"/miperfil"}>
+                <button className="botonperfil">
+                  <i class="fas fa-user-astronaut"></i>
+                </button>
+              </Link>
             </div>
           </div>
-          <div className="me-2">
-                <button
-                  className="noselect botonLogOut"
-                  onClick={() => {
-                    const desconecto = actions.LogOut();
-                    if (desconecto) {
-                      navigate(0);
-                    }
-                  }}
-                >
-                  <span className="text">
-                    {store.username ? store.username : ""}
-                  </span>
-                  <span className="icon">
-                    {" "}
-                    <i className="fas fa-sign-out-alt"></i>{" "}
-                  </span>
-                </button>
-              </div>
-              <div className="me-2">
-                <Link to={"/miperfil"}>
-                  <button className="botonperfil">
-                    <i class="fas fa-user-astronaut"></i>
-                  </button>
-                </Link>
-              </div>
         </div>
-      </div>
       ) : (
         <div className="container-fluid">
           <a
